359733798f679826699e3c502453b37ffc2c07b8
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-12 00:17:49 +0300

    reversed alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets in reversed order
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l <= 'a'; l--)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
