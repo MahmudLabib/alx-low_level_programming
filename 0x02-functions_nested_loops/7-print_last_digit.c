df69c8d4b2ac171fb3ead42b951ba77b12d1cc1c
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-16 01:50:24 +0300

    Print last digit of number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-int _abs(int num);
 /**
  * print_last_digit - Printing the last digit of a number
  * @num: Number to get it's last digit
@@ -9,17 +8,11 @@ int _abs(int num);
  */
 int print_last_digit(int num)
 {
-	int last_digit = _abs(num) % 10;
+	int absNum = (num < 0) ? -num : num;
+	int last_digit = absNum % 10;
 	char _d2char = last_digit + '0';
 
 	_putchar(_d2char);
 	return (last_digit);
 }
 
-int _abs(int num)
-{
-	if (num >= 0)
-		return (num);
-	else
-		return (-num);
-}
