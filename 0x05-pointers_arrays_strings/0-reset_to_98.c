90f6198a7725aa25f223311629517456d4732b36
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-21 17:43:16 +0300

    pass by reference

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function takes ptr2int and updates the value it points
+ * @n: pointer that holds the address of int number
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
