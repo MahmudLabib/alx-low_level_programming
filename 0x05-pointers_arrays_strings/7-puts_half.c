83a98f911d63aab6183628212252c7829916044d
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-22 03:51:03 +0300

    Print half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - Get the length of the string
+ * @s: Pointer to fist character
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+        int counter = 0;
+
+        while (*s != '\0')
+        {
+                counter++;
+                s++;
+        }
+        return (counter);
+}
+
+/**
  * puts_half - Prints half of a string
  * @str: Pointer to the first character of the string
  * 
