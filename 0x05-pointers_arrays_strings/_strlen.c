76b1c5bc7d5cd25f9676e6c742dea40e690cd677
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-22 03:44:42 +0300

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/_strlen.c
@@ -0,0 +1,11 @@
+int _strlen(char *s)
+{
+        int counter = 0;
+
+        while (*s != '\0')
+        {
+                counter++;
+                s++;
+        }
+        return (counter);
+}
