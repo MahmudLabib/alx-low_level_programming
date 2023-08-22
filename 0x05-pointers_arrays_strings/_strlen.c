c56af105b7e7ed2aeba904802e90aade5b2096e2
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-22 03:47:58 +0300

    prints half of a string

--- 0x05-pointers_arrays_strings/_strlen.c
+++ 0x05-pointers_arrays_strings/_strlen.c
@@ -1,3 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - Get the length of the string
+ * @s: Pointer to fist character
+ *
+ * Return: Length of string
+ */
+
 int _strlen(char *s)
 {
         int counter = 0;
