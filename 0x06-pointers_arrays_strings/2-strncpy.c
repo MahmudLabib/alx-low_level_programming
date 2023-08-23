be9b79b06e4e9be33ec455feb265c594a38efc86
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-24 02:38:19 +0300

    _strncpy copies string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i;
 
-	while (dest[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
-	while (j < n && src[j] != '\0')
-		dest[i++] = src[j++];
+	}
 	return (dest);
 }
