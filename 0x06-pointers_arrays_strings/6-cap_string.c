d402529d84ba717cb72d1b206a7b7f698f473165
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-24 23:08:42 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @target: destination string
+ * @source: source string
+ * @len: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *target, char *source, int len)
+{
+	int idx;
+
+	idx = 0;
+	while (source[idx] != '\0' && idx < len)
+	{
+		target[idx] = source[idx];
+		idx++;
+	}
+	while (idx < len)
+	{
+		target[idx] = '\0';
+		idx++;
+	}
+	return (target);
+}
