c32885a6222cd9a0a71eaf8e80e7c575822d6116
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-28 02:19:37 +0300

    encodes string to leet'1337'

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - Function that incodes a string into 1337
+ * @s: Pointer to first array character
+ *
+ * Return: Pointer to character
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char ch[11] = {'A', 'a', 'E', 'e' , 'O', 'o', 'T', 't', 'L', 'l', '\0'},
+	     str[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1', '\0'};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; str[j] != '\0'; j++)
+			if(s[i] == ch[j])
+				s[i] = str[j];
+		i++;
+	}
+	return (s);
+}
