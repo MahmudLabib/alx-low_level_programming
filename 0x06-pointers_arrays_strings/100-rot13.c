d6bb8fd3ff89d4cca2f28360844fc01ed09bd96f
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-28 04:02:55 +0300

    encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,7 +15,8 @@ char *rot13(char *s)
 		while ((s[i] >= 'a' && s[i] <= 'z') ||
 				(s[i] >= 'A' && s[i] <= 'Z'))
 		{
-			if (s[i] < 'm')
+			if ((s[i] >= 'a' && s[i] <= 'm') ||
+				(s[i] >= 'A' && s[i] <= 'M'))
 				s[i] += 13;
 			else
 				s[i] -= 13;
