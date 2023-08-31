fd2e197715e6f5f55ad5498d237ef618c7c410b9
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-31 03:26:36 +0300

    returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,6 +4,7 @@ int is_div(int n, int div);
 /**
  * is_prime_number - checks if a number is prime.
  * @n: input
+ *
  * Return: 1 or 0
  */
 int is_prime_number(int n)
@@ -18,9 +19,10 @@ int is_prime_number(int n)
 }
 
 /**
- * is_divisible - check if num is divisible
- * @num: the number to be checked
+ * is_div - check if num is divisible
+ * @n: the number to be checked
  * @div: the result of division
+ *
  * Return: 1 if num is divisible or 0 if numis not divisible
  */
 int is_div(int n, int div)
