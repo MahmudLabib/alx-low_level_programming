423e1930c18aa1cfa9d710bdbf1ee23e129db35d
MahmudLabib <mahmud.labiib@gmail.com>
2023-08-31 03:22:28 +0300

    returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+int is_div(int n, int div);
+
+/**
+ * is_prime_number - checks if a number is prime.
+ * @n: input 
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n <= 3)
+		return (1);
+
+	return (is_div(n, div));
+}
+
+/**
+ * is_divisible - check if num is divisible
+ * @num: the number to be checked
+ * @div: the result of division
+ * Return: 1 if num is divisible or 0 if numis not divisible
+ */
+int is_div(int n, int div)
+{
+	if (n % div == 0)
+	{
+		return (0);
+	}
+	if (div == n / 2)
+	{
+		return (1);
+	}
+	return (is_div(n, div + 1));
+}
