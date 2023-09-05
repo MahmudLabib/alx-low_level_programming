96e8792e6e15802947264d517d6c532551a8f0b2
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-05 20:47:11 +0300

    Multiplies two numbers form terminal command

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: No. of arguments
+ * @argv: Array of strings
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int n1 = atoi(argv[1]);
+		int n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
