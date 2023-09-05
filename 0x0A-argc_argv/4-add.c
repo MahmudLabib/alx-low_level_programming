bb698d3c7dbd0a820662ec8937b684cdb764a1b4
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-06 00:36:13 +0300

    by using agrc & argv prints add +nums

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - Adds positive numbers
+ * @argc: No. of arguments
+ * @argv: Array of strings
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int i;
+
+	for (i = argc - 1; i > 0; i--)
+	{
+		int n = atoi(argv[i]);
+
+		if (n > 0)
+		{
+			result += n;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	if (result != 0)
+		printf("%d\n", result);
+	return (0);
+}
+	
