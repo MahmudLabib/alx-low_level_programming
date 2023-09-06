6a682cf8e837044917b6487ef7ecb14385460dfd
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-07 02:32:18 +0300

    function allocates memory in heap, and initializes it with specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,11 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *cPtr = (char*) malloc(size * sizeof(char));
+	char *cPtr = (char *) malloc(size * sizeof(char));
 
 	if (size == 0)
+		return (NULL);
+	if (cPtr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
