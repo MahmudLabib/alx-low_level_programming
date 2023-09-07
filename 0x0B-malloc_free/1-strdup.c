148781cc5d290a83738d15e1d6d5e0abb97603bc
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-07 04:03:10 +0300

    function that returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_size - Get the size of the string literal
+ * @p: Pointer to address momory location
+ *
+ * Return: size of string
+ */
+int string_size(char *p)
+{
+	int i, counter = 0;
+
+	for (i = 0; p[i] != '\0'; i++)
+		counter++;
+	return (counter);
+}
+/**
+ * _strdup - Duplicate the string to heap momory
+ * @str: Pointer to address of the string be duplicated
+ *
+ * Return: pointer to momory address
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int size = string_size(str);
+	char *sPtr = (char *) malloc(size * sizeof(char));
+
+	if (sPtr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		sPtr[i] = str[i];
+	return (sPtr);
+}
