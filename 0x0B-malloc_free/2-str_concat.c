f15432dd78947e7ab97f60a93d1d75ab92df3a67
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-07 18:04:55 +0300

    concatenates two strings into newly allocated memory in heap section

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ int _sizeof(char *p)
 	int i, counter = 0;
 
 	if (p == NULL)
-		return ('\0');
+		return (0);
 	for (i = 0; p[i] != '\0'; i++)
 		counter++;
 	return (counter);
@@ -33,7 +33,13 @@ char *str_concat(char *s1, char *s2)
 	int s_total_size = s1_size + s2_size + 1;
 	int i, j = 0;
 	char *s = (char *) malloc(s_total_size * sizeof(char));
-
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; s[i] < s_total_size; i++)
 	{
 		if (i < s1_size)
