80fa70cc72c2b896d6af78ff8be68c7545e1a36b
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-14 01:52:01 +0300

    pointer function print array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate throw the array elements
+ * @array: Pointer to array
+ * @size: size of the array
+ * @action: Pointer to function
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
