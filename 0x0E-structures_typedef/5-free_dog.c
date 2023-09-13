48efb9b5e67b39989328dbc2113812fbab86f3cc
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-13 16:49:08 +0300

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,15 +4,15 @@
 
 /**
  * free_dog - function that free a structure
- * d: Pointer to structure
- *
+ * @d: pointer to dog
  * Return: void
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free(d);
+		free(d->name);
+		free(d->owner);
 		d = NULL;
 	}
 }
