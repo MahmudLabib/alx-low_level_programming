cc37f9c13de3cd40e1ab830159783e821c980186
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-22 02:16:26 +0300

    calc no. of elements in list_t list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - calc number of elements in a linked list_t list
+ * @h: Pointer to the head node of the list_t
+ *
+ * Return: No. of elements in the list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int node = 0;
+	while (h)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
