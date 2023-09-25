349c4c228d4d0fc3784adeaea3f2426d48ec5991
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-25 23:18:12 +0300

    prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,17 @@
 #include "lists.h"
+#include "unistd.h"
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: Pointer to the listint_t list
@@ -13,7 +25,7 @@ size_t print_listint(const listint_t *h)
 
 	while (pn)
 	{
-		_putchar(pn->n);
+		printf("%d\n", pn->n);
 		counter++;
 		pn = pn->next;
 	}
