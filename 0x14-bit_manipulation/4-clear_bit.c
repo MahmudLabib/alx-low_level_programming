ced99ddce7b4051e33bf99e5cbb4f93d9dcf1f7f
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-29 05:47:36 +0300

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-	if (*n &(1 << index))
+	if (*n & (1 << index))
 		*n ^= (1 << index);
 	return (1);
 }
