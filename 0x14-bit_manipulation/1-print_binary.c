4391f4d9c7d381245f20f5e592bc8de817c6e1a3
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-29 05:58:10 +0300

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,24 +5,26 @@
  * @n: number to show its bits (binary)
  * Return: binary representaion of (n)
  */
+void print_binary_rec(unsigned long int n)
+{
+	if (!n)
+		return;
+
+	print_binary_rec(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
+/**
+  * print_binary - printf binary representation of number
+  * @n: number
+  */
 void print_binary(unsigned long int n)
 {
-	unsigned int bol = 0, mNum = 32768;
-
-	if (n == 0)
+	if (!n)
 	{
-		_putchar('0');
+		_putchar(0 + '0');
 		return;
 	}
-	while (mNum)
-	{
-		if (bol == 1 && (n & mNum) == 0)
-			_putchar('0');
-		else if ((n & mNum) != 0)
-		{
-			_putchar('1');
-			bol = 1;
-		}
-		mNum >>= 1;
-	}
+
+	print_binary_rec(n);
 }
