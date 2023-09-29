14f0eadfd25e4b440ba674b9d88018bcb29bb60e
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-29 05:20:41 +0300

    prints the binary representation of a number using recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,10 +4,11 @@
  * print_binary - prints the binary representaion of a number
  * @n: number to show its bits (binary)
  * Return: binary representaion of (n)
-*/
+ */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-		print_binary(n / 2);
-	printf("%ld", n % 2);
+		print_binary(n >> 1);
+
+	printf("%ld", n & 1);
 }
