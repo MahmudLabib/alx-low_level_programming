3e2055c35e900972501aae689b985a2813498dd0
MahmudLabib <mahmud.labiib@gmail.com>
2023-11-11 10:34:28 +0200

    sum of all int data in dlist

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,3 @@
-// Write a function that returns the sum of all the data (n) of a dlistint_t linked list.
-
-// Prototype: int sum_dlistint(dlistint_t *head);
-// if the list is empty, return 0
 #include "lists.h"
 /**
  * sum_dlistint - calc the sum of all data(n) of a dlistint_t list
@@ -13,7 +9,7 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 	dlistint_t *ptrNode = head;
-	
+
 	if (!head)
 		return (0);
 	while (ptrNode)
